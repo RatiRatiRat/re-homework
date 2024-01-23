b51b735c7b1ea9c0346e37c3c83f26eeb3683584
Rati <sifrashvilirati@gmail.com>
2024-01-23 13:45:31 +0300

    JS homework

---
+++ js.homework/app.js
@@ -0,0 +1,143 @@
+// 1) დაწერეთ კოდი სადაც შეამოწმებთ დადებითია თუ უარყოფითი 
+// ეს რიცხვი!!
+
+//asnwer: 
+
+
+//2)დაწერეთ მარტივი კალკულატორი სადაც გამოიყენებთ ოპერატორებს(+, -, *, /)
+// switch case დახმარებით უბრალოდ დარწმუნდით რომ თითოეულ ქეისი ასრულებს ამ მეთოდებს 
+
+//asnwer: 
+
+//3) დაწერეთ იგივე კალკულატორი if else დახმარებით!!
+
+//asnwer: 
+
+//4) დაწერეთ ავტორიზაციის ლოგიკა სადაც უნდა დავარეგისტრიროთ User
+// user უნდა შემოყავდეს firstName,lastName,email,password,phoneNumber
+// firstName,lastName,email,password არის სავალდებულო ველები
+// ამიტომ ერთიანად შეამოწმეთ ეს ოთხი ველი და დარწმუნდით თუ ცარიელი input 
+// არარის მაშინ დალოგეთ რომ შენ დარეგისტრირდი წარმატებით თუ ცარიელია მაშინ
+// დალოგეთ "input is required" გამოიყენეთ if else
+
+//asnwer:
+
+//1)
+
+
+// let number = 44
+
+
+// if (number>0) {
+
+//     console.log("positive number")
+
+// }
+
+// else {
+//     console.log("negative number")
+// }
+
+
+
+
+
+
+
+
+
+
+//2)
+
+
+
+
+// let input = 44
+
+
+// switch (input) {
+//     case "-":
+
+//     console.log(44-0)
+        
+//     break;
+
+//     case "+":
+
+//     console.log(44+44)
+        
+//     break;
+
+//     case "/":
+
+//     console.log(44/0)
+        
+//     break;
+
+//     case "*":
+
+//     console.log(44*3)
+        
+//     break;
+// }
+
+
+
+
+//3)
+
+
+// let input = "+"
+
+
+// if (input="+") {
+    
+//     console.log(44+0)
+
+// }
+
+
+// else if (input="-") {
+    
+//     console.log(44-1)
+
+// }
+
+
+// else if (input="*"){
+    
+//     console.log(44*2)
+
+// }
+
+
+// else {
+//     console.log(44/2)
+// }
+    
+ 
+
+
+// let firstName = "Rati"
+// let lastName  = "Sifrashvili"
+// let Email     = "Sifrashvili@gmail.com"
+// let password  = "Rati123"
+// let phoneNumber = "+40 32077790"
+
+
+// if  (
+
+//      firstName !== "" &&
+//      lastName  !== "" &&
+//      Email     !== "" &&
+//      password  !== "" &&
+//      phoneNumber !== "" 
+
+    
+// ) {
+//     console.log("Succesfull Registration")
+// }
+//   else  { 
+//     console.log("input is required" );}
+  
+
